027f7159b6202d2d619ac08c62da2cf2ecae9d64
Walt <walt.yaoza@gmail.com>
2025-01-31 21:19:58 -0800

    dropdown

--- src/controllers/chain.controller.ts
+++ src/controllers/chain.controller.ts
@@ -6,9 +6,10 @@ import chainSubgraphService from "../services/chain.subgraph.service";
 
 // List of token addresses to prepopulate the dropdown
 const tokenAddresses: string[] = [
-  "0x1151cb3d861920e07a38e03eead12c32178567f6", // 
-  "0xad2afccec0384f272f430e763041f0ba4decbe27", // Trump
+  "0x4256424ec9239a4ab069aaece123919ef263fe7e", // GDOG
+  "0x866a1a2c2ff8230dc44c5433f93ef478f6c8afcc", // LuckyGem
   "0x146d0fff5be0c7825f47ca0359c3070b12a4684b", // Alpha
+  "0xd29da236dd4aac627346e1bba06a619e8c22d7c5", // MAGA
 
 ];
 
@@ -18,7 +19,7 @@ const getTokens = catchAsync(async (req: Request, res: Response): Promise<Respon
     tokenAddresses.map(async (tokenAddress) => {
       const pools = await chainSubgraphService.reloadPools(tokenAddress);
       
-      if (!pools || pools.length === 0) {
+      if (pools == undefined || pools.length === 0) {
         return res.status(404).json({ message: `No pools found for token address ${tokenAddress}` });
       }
       
